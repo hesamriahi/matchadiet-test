8e584bcde01797f04d666f7db89dcef79dd54891
riahi <h.riahi017@gmail.com>
2025-04-13 01:23:55 +0330

    migrate tailwind version to 3

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,15 +1,19 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="fa">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @livewireStyles
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <title>{{ $title ?? 'Page Title' }}</title>
-    </head>
-    <body>
-        {{ $slot }}
-        @livewireScripts
-    </body>
+    <title>{{ $title ?? 'Page Title' }}</title>
+
+</head>
+<body>
+
+    {{ $slot }}
+    {{--        @livewireScripts--}}
+
+</body>
 </html>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,5 +1,9 @@
 <x-slot:title>this is title</x-slot>
 
 <div>
-    <h1>Hello World</h1>
+    <header class="sticky top-0 z-10">
+        <section class="bg-gray-700 max-w-4xl text-red mx-auto p-4 flex justify-between items-center">
+            <h1 class="text-3xl font-medium text-gray-500">matchadiet</h1>
+        </section>
+    </header>
 </div>
